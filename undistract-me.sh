ca5067cf0c32b0223da90a417f85419de158f6d4
Thomas Wayne McCarthy <twmccart@users.noreply.github.com>
2017-10-17 14:32:18 -0400

    changed undistract-me.sh script so that it doesn't require installation in /usr/share/

--- undistract-me.sh
+++ undistract-me.sh
@@ -2,6 +2,11 @@
 #
 # Check for interactive bash and that we haven't already been sourced.
 [ -z "$BASH_VERSION" -o -z "$PS1" -o -n "$last_command_started_cache" ] && return
-
-. /usr/share/undistract-me/long-running.bash
+#echo "script is $0"
+#directory="$(dirname "$(readlink -f "$0")")"
+#directory="$(dirname "$BASH_SOURCE")"
+directory="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#echo "directory is: ${directory}"
+#echo "directory should be '~/.local/src/undistract-me'"
+. ${directory}/long-running.bash
 notify_when_long_running_commands_finish_install
